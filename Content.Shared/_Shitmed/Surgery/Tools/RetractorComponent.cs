079800c6e08c941f0029df4db462b312a54f42e9
JunJun <nekitnea@gmail.com>
2026-03-24 17:05:13 +0300

    Ru Locale Add (#2)
    * Localization Textures
    
    * Localization Code
    
    * Locale FTL (Old)
    
    * Locale Proto
    
    * Fix YAML locale
    
    * fixing errors
    
    * Fix Meta
    
    * Fix FTL
    
    * Ftl fix
    
    * fix emotes
    
    * fix locale
    
    * Fix Emote

--- Content.Shared/_Shitmed/Surgery/Tools/RetractorComponent.cs
+++ Content.Shared/_Shitmed/Surgery/Tools/RetractorComponent.cs
@@ -5,8 +5,12 @@ namespace Content.Shared._Shitmed.Medical.Surgery.Tools;
 [RegisterComponent, NetworkedComponent]
 public sealed partial class RetractorComponent : Component, ISurgeryToolComponent
 {
-    public string ToolName => "a retractor";
+    public string ToolName => Loc.GetString("surgery-tool-retractor"); // Ru-Localization
     public bool? Used { get; set; } = null;
+
+    /// <summary>
+    ///     Multiply the step's doafter by this value.
+    /// </summary>
     [DataField]
     public float Speed { get; set; } = 1f;
 }
